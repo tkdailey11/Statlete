492341766342d86fc69541e1cea84f7114251078
Taylor deJonge <tmdejonge7@gmail.com>
2018-04-09 17:00:45 -0600

    added view existing stats vc

---
+++ mobile/Statlete Mobile App/Statlete Mobile App/VEGamesViewController.swift
@@ -0,0 +1,36 @@
+//
+//  VEGamesViewController.swift
+//  Statlete Mobile App
+//
+//  Created by Taylor M de Jonge on 4/9/18.
+//  Copyright © 2018 Taylor M de Jonge. All rights reserved.
+//
+
+import UIKit
+
+class VEGamesViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
