4fe5b554d81e50633500ad878d4627b9b226e442
Taylor deJonge <tmdejonge7@gmail.com>
2018-04-11 21:23:55 -0600

    segue to player card modally

--- mobile/Statlete Mobile App/Statlete Mobile App/Sportfolio/PlayerInTeamViewController.swift
+++ mobile/Statlete Mobile App/Statlete Mobile App/Sportfolio/PlayerInTeamViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class PlayerInTeamViewController: UIViewController {
 
+    @IBAction func dismissPopup(_ sender: UIButton) {
+         dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
