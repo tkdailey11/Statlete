486afba039784746d967a44da3b1907322a9b04e
Joseph Pugliano <joe@josephs-air.uconnect.utah.edu>
2018-04-17 16:11:58 -0600

    Joe's commit

--- web/src/components/TeamStats.vue
+++ web/src/components/TeamStats.vue
@@ -1,6 +1,54 @@
 <template>
   <div id="TeamStats">
     <button @click="goBack">Go Back</button>
+<!--
+Code here
+-->
+    <table id="Stats">
+      <thead>
+        <th />
+        <th>
+          Assists
+        </th>
+        <th>
+          Corners
+        </th>
+        <th>
+          Crosses
+        </th>
+        <th>
+          Fouls
+        </th>
+        <th>
+          Goals
+        </th>
+        <th>
+          Offsides
+        </th>
+        <th>
+          Yellow Cards
+        </th>
+        <th>
+          Red Cards
+        </th>
+        <th>
+          Saves
+        </th>
+        <th>
+          Shots
+        </th>
+        <th>
+          Shots On Goal
+        </th>
+      </thead>
+
+      <tr v-for="i in 10">
+          {{'Player ' + i}}
+        <td v-for="i in 11">
+          {{i}}
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -11,6 +59,9 @@
       goBack: function() {
         this.$emit('TeamStatsClose');
         console.log('CLOSE');
+      },
+      cellClick: function(x) {
+        console.log(x);
       }
     }
   }
@@ -21,4 +72,31 @@
     width: 100%;
     background-color: orange;
   }
+  #MyBox{
+    width: 100px;
+    height: 100px;
+    background-color: red;
+    float: left;
+    margin: 10px;
+  }
+  #Stats {
+    min-width: 300px;
+    min-height: 300px;
+    background-color: purple;
+    color: white;
+    border-style: solid;
+    border-color: white;
+    border-width: medium;
+  }
+  tr td {
+    border-style: solid;
+    border-color: white;
+    border-width: medium;
+  }
+  th {
+    border-style: solid;
+    border-color: white;
+    border-width: medium;
+  }
+
 </style>
