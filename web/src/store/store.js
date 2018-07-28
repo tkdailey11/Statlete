29362b8d47905f8e3acfbf509b6387d36c9828e0
Tyler Dailey <tkdailey11@gmail.com>
2018-07-27 19:03:02 -0600

    Implemented store stuff for Main

--- web/src/store/store.js
+++ web/src/store/store.js
@@ -5,6 +5,82 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        value: 11
+        loggedInUser: '',
+        currentUserEmail: '',
+        currTeam: {
+          id: '',
+          name: '',
+          token: ''
+        },
+        activeGameId: '',
+        selectedSport: 'basketball',
+        selectedTeam: {
+          id: '',
+          name: '',
+          token: ''
+        },
+        players: []
+    },
+    getters: {
+      loggedInUser: state => {
+        return state.loggedInUser
+      },
+      currentUserEmail: state => {
+        return state.currentUserEmail
+      },
+      selectedTeamId: state => {
+        return state.selectedTeam.id
+      },
+      selectedTeamName: state => {
+        return state.selectedTeam.name
+      },
+      selectedTeamToken: state => {
+        return state.selectedTeam.token
+      },
+      teamID: state => {
+        return state.currTeam.id
+      },
+      teamName: state => {
+        return state.currTeam.name
+      },
+      teamToken: state => {
+        return state.currTeam.token
+      },
+      selectedSport: state => {
+        return state.selectedSport
+      },
+      activeGameId: state => {
+        return state.activeGameId
+      },
+      players: state => {
+        return state.players
+      }
+    },
+    mutations: {
+      SET_LOGGED_IN_USER: (state, payload) => {
+        state.loggedInUser = payload;
+        state.currentUserEmail = state.loggedInUser.email
+      },
+      SET_SELECTED_TEAM: (state, payload) => {
+        state.selectedTeam = payload
+      },
+      SET_SELECTED_TEAM_ID: (state, payload) => {
+        state.selectedTeam.id = payload
+      },
+      SET_CURR_TEAM: (state, payload) => {
+        state.currTeam = payload
+      },
+      SET_SELECTED_SPORT: (state, payload) => {
+        state.selectedSport = payload
+      },
+      SET_ACTIVE_GAME_ID: (state, payload) => {
+        state.activeGameId = payload
+      },
+      SET_PLAYERS: (state, payload) => {
+        state.players = payload
+      },
+      APPEND_PLAYER: (state, payload) => {
+        state.players.push(payload)
+      }
     }
 });
